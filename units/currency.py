715535f14b3c229dfa6b1ced5b356ea7d1de44a0
Kirill Maltsev <maltsevkirill@gmail.com>
2016-03-06 15:37:57 +0200

    Update exchange rates

--- units/currency.py
+++ units/currency.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 USD = (1,      [u'$',    'US dollar', 'US dollars', 'American dollar', 'American dollars'])
-EUR = (1.09009647354, [u'€',    'euro', 'euros'])
-JPY = (0.00877500877501, [u'¥',    'yen', 'yens'])
-GBP = (1.40775673964, [u'£',    'pound sterling', 'pounds sterling'])
-CHF = (1.0056315366, [u'Fr',   'Swiss franc', 'Swiss francs', 'franc', 'francs', 'Fr.'])
-CAD = (0.744379931517, [u'C$',   'Canadian dollar', 'Canadian dollars'])
-RUB = (0.0136119240455, [u'₽',    'Russian ruble', 'Russian rubles', 'ruble', 'rubles', 'rouble', 'roubles'])
-CNY = (0.152893509671, [u'yuan', 'yuan', 'yuans'])
+EUR = (1.09699642379, [u'€',    'euro', 'euros'])
+JPY = (0.00879198171268, [u'¥',    'yen', 'yens'])
+GBP = (1.41578887756, [u'£',    'pound sterling', 'pounds sterling'])
+CHF = (1.00660331776, [u'Fr',   'Swiss franc', 'Swiss francs', 'franc', 'francs', 'Fr.'])
+CAD = (0.745378652355, [u'C$',   'Canadian dollar', 'Canadian dollars'])
+RUB = (0.0136838216177, [u'₽',    'Russian ruble', 'Russian rubles', 'ruble', 'rubles', 'rouble', 'roubles'])
+CNY = (0.153468385513, [u'yuan', 'yuan', 'yuans'])
 
 
 _BASE = USD
