062b01478409402c7b9d7d801133c1c3f0eca908
Kirill Maltsev <maltsevkirill@gmail.com>
2016-03-07 13:28:18 +0200

    Add new aliases to speed and time units

--- units/speed.py
+++ units/speed.py
@@ -10,8 +10,8 @@ _HOUR_VALUE = float(_t.HOUR[0])
 _SECOND_VALUE = float(_t.SECOND[0])
 
 METER_PER_SECOND =   (      _l.METER[0] / _SECOND_VALUE, ['m/s'] + _getNames('per second', _l.METER[1]))
-KILOMETER_PER_HOUR = (    _l.KILOMETER[0] / _HOUR_VALUE, ['km/h'] + _getNames('per hour', _l.KILOMETER[1]))
-MILE_PER_HOUR =      (         _l.MILE[0] / _HOUR_VALUE, ['mph'] + _getNames('per hour', _l.MILE[1]))
+KILOMETER_PER_HOUR = (    _l.KILOMETER[0] / _HOUR_VALUE, ['km/h'] + _getNames('per hour', _l.KILOMETER[1]) + ['km/hr'])
+MILE_PER_HOUR =      (         _l.MILE[0] / _HOUR_VALUE, ['mph'] + _getNames('per hour', _l.MILE[1]) + ['mi/h', 'mi/hr'])
 KNOT =               (_l.NAUTICAL_MILE[0] / _HOUR_VALUE, ['kn', 'knot', 'knots', 'kt'])
 FOOT_PER_SECOND =    (       _l.FOOT[0] / _SECOND_VALUE, ['ft/s'] + _getNames('per second', _l.FOOT[1]))
 
--- units/time.py
+++ units/time.py
@@ -2,7 +2,7 @@
 MILLISECOND = (           0.001,   ['ms', 'millisecond', 'milliseconds', 'millisec', 'msec'])
 SECOND =      (           1,       ['s', 'second', 'seconds', 'sec'])
 MINUTE =      (          60,       ['min', 'minute', 'minutes'])
-HOUR =        (       60*60,       ['h', 'hour', 'hours'])
+HOUR =        (       60*60,       ['h', 'hour', 'hours', 'hr'])
 DAY =         (HOUR[0] * 24,       ['d', 'day', 'days'])
 WEEK =        (  DAY[0] * 7,       ['wk', 'week', 'weeks'])
 MONTH =       (DAY[0] * 30.436875, ['mon', 'month', 'months'])
