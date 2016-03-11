1be4124018a42a4a784a6546c8fb01f18837465c
Kirill Maltsev <maltsevkirill@gmail.com>
2016-03-12 00:49:31 +0200

    Disable debug mode on production

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 import webapp2
 from controller.WebHook import WebHook
 
+debug = os.path.exists('./tests')
+os.environ['DEBUG'] = '1' if debug else ''
+
 app = webapp2.WSGIApplication([
     ('/api/webhook', WebHook),
-], debug=True)
+], debug=debug)
