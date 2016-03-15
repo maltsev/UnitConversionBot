959a6d60e2bc066925bc59943c0b5917575e3852
Kirill Maltsev <maltsevkirill@gmail.com>
2016-03-15 16:00:15 +0200

    Change convert script's output

--- scripts/convert.py
+++ scripts/convert.py
@@ -21,6 +21,6 @@ for expression in expressions:
     try:
         units = parseExpression(parseMessageText(expression)['expression'], unitsIndex)
         toValueUnit = convertUnit(units['fromValueUnit'], units['toUnit'])
-        print formatValueUnit(toValueUnit).encode('utf-8')
+        print u'{}: {}'.format(expression, formatValueUnit(toValueUnit)).encode('utf-8')
     except Exception as error:
-        print u'{:32s} {}\n'.format(error.__class__.__name__, expression).encode('utf-8')
+        print u'{:32s} {}'.format(error.__class__.__name__, expression).encode('utf-8')
