22183fd86b6b009aa1e4c5408b5031e82770971a
Kirill Maltsev <maltsevkirill@gmail.com>
2019-07-03 15:42:31 +0200

    Refactor composite names generation
    For example, km/g

--- units/density.py
+++ units/density.py
@@ -3,36 +3,49 @@ from __future__ import division as _
 import volume as _v
 import length as _l
 import mass as _m
+from modules.helpers import generateCompositeNames as _getNames
 
 
-def _getNames(massNames, volumeNames, lengthNames=[]):
-    volumeShortName = volumeNames[0]
-    massShortName = massNames[0]
-    names = [
-        massShortName + '/' + volumeShortName, # kg/m³
-    ]
+GRAM_PER_CENTIMETER_CUBIC = (
+    _m.GRAM[0] / _v.CENTIMETER_CUBIC[0],
+    _getNames(_m.GRAM[1], _v.CENTIMETER_CUBIC[1] + _l.CENTIMETER[1]),
+    'KILOGRAM_PER_METER_CUBIC'
+)
 
-    volumeAllNames = volumeNames[1:] + [volumeShortName] + lengthNames
-    massAllNames = massNames[1:] + [massShortName]
-    # Full name ("kilogram per cubic meter") must be at the start
-    for massName in massAllNames:
-        for volumeName in volumeAllNames:
-            names.append(massName + ' per ' + volumeName)
-            if ' ' not in massName and ' ' not in volumeName:
-                names.append(massName + '/' + volumeName)
+GRAM_PER_MILLILITER = (
+    _m.GRAM[0] / _v.MILLILITER[0],
+    _getNames(_m.GRAM[1], _v.MILLILITER[1]),
+    'KILOGRAM_PER_METER_CUBIC'
+)
 
-    return names
+GRAM_PER_METER_CUBIC = (
+    _m.GRAM[0] / _v.METER_CUBIC[0],
+    _getNames(_m.GRAM[1], _v.METER_CUBIC[1] + _l.METER[1]),
+    'KILOGRAM_PER_METER_CUBIC'
+)
 
+KILOGRAM_PER_METER_CUBIC = (
+    _m.KILOGRAM[0] / _v.METER_CUBIC[0],
+    _getNames(_m.KILOGRAM[1], _v.METER_CUBIC[1] + _l.METER[1]),
+    'GRAM_PER_CENTIMETER_CUBIC'
+)
 
-GRAM_PER_CENTIMETER_CUBIC = (_m.GRAM[0] / _v.CENTIMETER_CUBIC[0], _getNames(_m.GRAM[1], _v.CENTIMETER_CUBIC[1], _l.CENTIMETER[1]), 'KILOGRAM_PER_METER_CUBIC')
-GRAM_PER_MILLILITER =       (_m.GRAM[0] / _v.MILLILITER[0],       _getNames(_m.GRAM[1], _v.MILLILITER[1]), 'KILOGRAM_PER_METER_CUBIC')
-GRAM_PER_METER_CUBIC =      (_m.GRAM[0] / _v.METER_CUBIC[0],  _getNames(_m.GRAM[1], _v.METER_CUBIC[1], _l.METER[1]), 'KILOGRAM_PER_METER_CUBIC')
+KILOGRAM_PER_LITER = (
+    _m.KILOGRAM[0] / _v.LITER[0],
+    _getNames(_m.KILOGRAM[1], _v.LITER[1]),
+    'KILOGRAM_PER_METER_CUBIC'
+)
 
-KILOGRAM_PER_METER_CUBIC = (_m.KILOGRAM[0] / _v.METER_CUBIC[0],  _getNames(_m.KILOGRAM[1], _v.METER_CUBIC[1], _l.METER[1]), 'GRAM_PER_CENTIMETER_CUBIC')
-KILOGRAM_PER_LITER =       (_m.KILOGRAM[0] / _v.LITER[0],        _getNames(_m.KILOGRAM[1], _v.LITER[1]), 'KILOGRAM_PER_METER_CUBIC')
+TONNE_PER_METER_CUBIC = (
+    _m.TONNE[0] / _v.METER_CUBIC[0],
+    _getNames(_m.TONNE[1], _v.METER_CUBIC[1] + _l.METER[1]),
+    'KILOGRAM_PER_METER_CUBIC'
+)
 
-TONNE_PER_METER_CUBIC = (_m.TONNE[0] / _v.METER_CUBIC[0],     _getNames(_m.TONNE[1], _v.METER_CUBIC[1], _l.METER[1]), 'KILOGRAM_PER_METER_CUBIC')
-
-POUND_PER_IMPERIAL_GALLON = (_m.POUND[0] / _v.IMPERIAL_GALLON[0], _getNames(_m.POUND[1], _v.IMPERIAL_GALLON[1]), 'KILOGRAM_PER_METER_CUBIC')
+POUND_PER_IMPERIAL_GALLON = (
+    _m.POUND[0] / _v.IMPERIAL_GALLON[0],
+    _getNames(_m.POUND[1], _v.IMPERIAL_GALLON[1]),
+    'KILOGRAM_PER_METER_CUBIC'
+)
 
 _BASE = 'KILOGRAM_PER_METER_CUBIC'
--- units/speed.py
+++ units/speed.py
@@ -2,10 +2,7 @@
 from __future__ import division as _
 import length as _l
 import time as _t
-
-
-def _getNames(base, lengthNames):
-    return [name + ' ' + base for name in lengthNames[1:]]
+from modules.helpers import generateCompositeNames as _getNames
 
 
 _SECOND_VALUE = float(_t.SECOND[0])
@@ -13,21 +10,21 @@ _MINUTE_VALUE = float(_t.MINUTE[0])
 _HOUR_VALUE = float(_t.HOUR[0])
 
 
-METER_PER_SECOND = (_l.METER[0] / _SECOND_VALUE, ['m/s'] + _getNames('per second', _l.METER[1]), 'KILOMETER_PER_HOUR')
-METER_PER_MINUTE = (_l.METER[0] / _MINUTE_VALUE, ['m/min'] + _getNames('per minute', _l.METER[1]), 'METER_PER_SECOND')
-METER_PER_HOUR =   (_l.METER[0] / _HOUR_VALUE,   ['m/h'] + _getNames('per hour', _l.METER[1]) + ['m/hr'], 'KILOMETER_PER_HOUR')
+METER_PER_SECOND = (_l.METER[0] / _SECOND_VALUE,  _getNames(_l.METER[1], _t.SECOND[1]), 'KILOMETER_PER_HOUR')
+METER_PER_MINUTE = (_l.METER[0] / _MINUTE_VALUE, _getNames(_l.METER[1], _t.MINUTE[1]), 'METER_PER_SECOND')
+METER_PER_HOUR =   (_l.METER[0] / _HOUR_VALUE,   _getNames(_l.METER[1], _t.HOUR[1]), 'KILOMETER_PER_HOUR')
 
-KILOMETER_PER_SECOND = (_l.KILOMETER[0] / _SECOND_VALUE, ['km/s'] + _getNames('per second', _l.KILOMETER[1]), 'KILOMETER_PER_HOUR')
-KILOMETER_PER_MINUTE = (_l.KILOMETER[0] / _MINUTE_VALUE, ['km/min'] + _getNames('per minute', _l.KILOMETER[1]), 'KILOMETER_PER_HOUR')
-KILOMETER_PER_HOUR =   (_l.KILOMETER[0] / _HOUR_VALUE,   ['km/h'] + _getNames('per hour', _l.KILOMETER[1]) + ['km/hr', 'kmph', 'kmh', 'kph'], 'METER_PER_SECOND')
+KILOMETER_PER_SECOND = (_l.KILOMETER[0] / _SECOND_VALUE, _getNames(_l.KILOMETER[1], _t.SECOND[1]), 'KILOMETER_PER_HOUR')
+KILOMETER_PER_MINUTE = (_l.KILOMETER[0] / _MINUTE_VALUE, _getNames(_l.KILOMETER[1], _t.MINUTE[1]), 'KILOMETER_PER_HOUR')
+KILOMETER_PER_HOUR =   (_l.KILOMETER[0] / _HOUR_VALUE,   _getNames(_l.KILOMETER[1], _t.HOUR[1]) + ['kmph', 'kmh', 'kph'], 'METER_PER_SECOND')
 
-MILE_PER_SECOND = (_l.MILE[0] / _SECOND_VALUE, ['mi/s'] + _getNames('per second', _l.MILE[1]), 'KILOMETER_PER_HOUR')
-MILE_PER_MINUTE = (_l.MILE[0] / _MINUTE_VALUE, ['mi/min'] + _getNames('per minute', _l.MILE[1]), 'KILOMETER_PER_HOUR')
-MILE_PER_HOUR =   (_l.MILE[0] / _HOUR_VALUE,   ['mph'] + _getNames('per hour', _l.MILE[1]) + ['mi/h', 'mi/hr', 'mp/h'], 'KILOMETER_PER_HOUR')
+MILE_PER_SECOND = (_l.MILE[0] / _SECOND_VALUE, _getNames(_l.MILE[1], _t.SECOND[1]), 'KILOMETER_PER_HOUR')
+MILE_PER_MINUTE = (_l.MILE[0] / _MINUTE_VALUE, _getNames(_l.MILE[1], _t.MINUTE[1]), 'KILOMETER_PER_HOUR')
+MILE_PER_HOUR =   (_l.MILE[0] / _HOUR_VALUE,   _getNames(_l.MILE[1], _t.HOUR[1], 'mph'), 'KILOMETER_PER_HOUR')
 
-FOOT_PER_SECOND = (_l.FOOT[0] / _SECOND_VALUE, ['ft/s'] + _getNames('per second', _l.FOOT[1]), 'METER_PER_SECOND')
-FOOT_PER_MINUTE = (_l.FOOT[0] / _MINUTE_VALUE, ['ft/min'] + _getNames('per minute', _l.FOOT[1]), 'METER_PER_SECOND')
-FOOT_PER_HOUR =   (_l.FOOT[0] / _HOUR_VALUE,   ['ft/h'] + _getNames('per hour', _l.FOOT[1]) + ['ft/hr'], 'METER_PER_HOUR')
+FOOT_PER_SECOND = (_l.FOOT[0] / _SECOND_VALUE, _getNames(_l.FOOT[1], _t.SECOND[1]), 'METER_PER_SECOND')
+FOOT_PER_MINUTE = (_l.FOOT[0] / _MINUTE_VALUE, _getNames(_l.FOOT[1], _t.MINUTE[1]), 'METER_PER_SECOND')
+FOOT_PER_HOUR =   (_l.FOOT[0] / _HOUR_VALUE,   _getNames(_l.FOOT[1], _t.HOUR[1]), 'METER_PER_HOUR')
 
 KNOT = (_l.NAUTICAL_MILE[0] / _HOUR_VALUE, ['kn', 'knot', 'knots', 'kt'], 'KILOMETER_PER_HOUR')
 
